e845f3cc17937f686b2fad574b3351f8f9775736
Katie Higgins <khiggins115@gmail.com>
2017-11-08 18:35:41 -0600

    updated js file

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 /* SET UP LEAFLET MAP */
 
-console.log("hi from before the map!!!");
+console.log("Hi there! This is just to demonstrate that we've retrieved the coefficients and appended them to the created JSON file!");
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoia2hpZ2dpbnMxMTUiLCJhIjoiY2ltcW9pZXZkMDBua3ZsbTRieXh1NmdkdSJ9.CDeDgVkdUyZS3nkyJWYAXg';
 var map = L.mapbox.map('map', 'mapbox.streets')
@@ -16,7 +16,11 @@ function getNumber(str){
 
 
 var colVals = [4000,6000,8000,10000,12000,14000,18000,24000,30000,60000];
+console.log("Here is an array of column values from the coefficient lookup table");
+console.log(colVals);
 var rowVals = [500,1000,1500,2000,2500,3000,3500,4000,5000,9000];
+console.log("Here is an array of row values from the coefficient lookup table");
+console.log(rowVals);
 //var landAreTable = new Array();
 
 
@@ -32,11 +36,12 @@ var parcelData, coeffData, coeffVal;
 
 /* GET DATA */
 function getData(){
-  //console.log("in getData!");
+  console.log("first we loop through the geojson data!");
   d3.json('data/parcelSubset2.geojson', function(data) {
     //console.log("got PARCEL data");
     parcelData = data;
     //console.log(parcelData.features[0]);
+    console.log("now we're looping through a csv of coefficients. Each one corresponds to a land bin (range of values)");
     d3.csv('data/land_binCoeff.csv', function(coeffs) {
       coeffData = coeffs;
       //console.log("got coeffs!");
@@ -49,6 +54,7 @@ function getData(){
 
         for(j=0; j < rowVals.length; j++){
           //get the row for reference
+
           if(livingArea >= rowVals[j] && livingArea < rowVals[j+1]){
             var refRow = j;
           }
@@ -56,10 +62,12 @@ function getData(){
 
         for(k=0; k < colVals.length; k++){
           //get column for reference
+
           if(landArea >= colVals[k] && landArea < colVals[k+1]){
             var refCol = k;
           }
         }//end get colVals
+
 
         if(refCol == 0){
             coeffVal = coeffData[refRow].land_bin1;
@@ -86,7 +94,10 @@ function getData(){
         parcelData.features[i].properties.coeff = getNumber(coeffVal);
 
       }
-      console.log("hi from after get coeffs!");
+      console.log("next we get row values based on conditions: if living area is between two numbers, assign that i value to variable refRow.");
+      console.log("Then we get column values based on similar logic, assigning i value to variable refCol");
+      console.log("then some if/else if statements; if ref col equals a certain value, then the coefficient value is equal to the coefficient data at index equal to refRow");
+      console.log("This is the end of the coefficient logic explaination!");
 
 
     });
